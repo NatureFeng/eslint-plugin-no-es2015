592f1b53178ac4b535d1deefd684fe0034f3e09e
冯天然 <fengtianran@meituan.com>
2017-01-03 19:44:38 +0800

    add new rule: no-default-function-params

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     'only-var': require('./lib/rules/only-var'),
     'no-class': require('./lib/rules/no-class'),
+    'no-default-params': require('./lib/rules/no-default-params'),
     'no-arrow-func': require('./lib/rules/no-arrow-func'),
     'no-import': require('./lib/rules/no-import'),
     'no-template-string': require('./lib/rules/no-template-string')
